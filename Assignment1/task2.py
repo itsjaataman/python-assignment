f374d13f6ce42dbf0fecee0bfdabb42809898083
Aman <itsjaataman@outlook.com>
2025-10-15 22:13:37 +0530

    Assignment 1

---
+++ Assignment1/task2.py
@@ -0,0 +1,8 @@
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+
+full_name = first_name + " " + last_name
+
+
+print(f"Hello, {full_name}! Welcome to the world of Python programming 😊")
